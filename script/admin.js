2c58f35c5ba9479e01129209c1611a2b4d807e9d
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-07 10:17:17 +0200

    theresalot

--- script/admin.js
+++ script/admin.js
@@ -3,16 +3,17 @@ date.textContent = new Date().getFullYear()
 
 let fineWrapper = document.querySelector('[data-tableAdmin]')
 //let Admin = JSON.parse(localStorage.getItem('products'))
-
+  
+// function to display products on a new page 
  
-function ViewAdmin(product){
-    fineWrapper.innerHTML = "";
+function ViewAdmin(product){  
+    fineWrapper.innerHTML = " ";
     try{
         let Admin = JSON.parse(localStorage.getItem('products'))
     }
     catch{
-            products.forEach(product=>{
-                fineWrapper.innerHTML +=`
+            products.forEach(product, i=>{
+                textContent.innerHTML +=`
                 <table class="table">
   <thead>
     <tr>
@@ -32,30 +33,43 @@ function ViewAdmin(product){
     </svg>Add</button>
     <button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
     <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
-  </svg>Delete</button></td>   
+  </svg>Delete</button></td>
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">New Product Modal</button>
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">New Product</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Image:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Product Type</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Price:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Add New item</button>
+      </div>
+    </div>
+  </div>
+</div>
+   
    ` 
-            })}  
-        }
+            })} 
+            
+            
 
-
-//         } else{
-//             fineWrapper.innerHTML += `<div class="d-flex justify-content-center">
-//             <div class="spinner-border" role="status">
-//               <span class="visually-hidden">Loading...</span>
-//             </div>
-//           </div>`           // add spinner
-//     }
-    
-        
-//     } catch(e){
-//         fineWrapper.innerHTML = `Try again`
-//     }
-// }
-
-// let NewProducts = [{
-//     image: " ",
-//     productType: " ",
-//     price: " ",
-
-// }]
-ViewAdmin()
