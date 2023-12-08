5e3837ed2eb0acbde31293a6694d93246ab3353f
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-08 08:43:22 +0200

    checkout page--stillneedtofinish

--- script/checkout.js
+++ script/checkout.js
@@ -2,116 +2,56 @@ let date = document.querySelector("[data-year]");
 date.textContent = new Date().getFullYear();
 
 const tableDisplay = document.querySelector("[data-checkout]");
-let table = JSON.parse(localStorage.getItem("cart"));
-let products =
-  JSON.parse(localStorage.getItem("products")) ||
-  localStorage.setItem(
-    "products",
-    JSON.stringify([
-      {
-        id: "1",
-        productType: "Cream",
-        Price: 450,
-        description: "Used day and night",
-        image:
-          " https://i.postimg.cc/zvbhjbnh/cream-stylized-product-opt-540x.webp",
-      },
-      {
-        id: "2",
-        productType: "Cleanser",
-        Price: 550,
-        description: "Used day and night",
-        image: "https://i.postimg.cc/xC0qsszM/goji-berry-clenser-opt-900x.webp",
-      },
-      {
-        id: "3",
-        productType: "Vitamin C Serum",
-        Price: 560,
-        description: "Used day and night",
-        image:
-          "https://i.postimg.cc/3NbX9pCC/vitamin-c-serum-stylized-2-opt-900x.webp",
-      },
-      {
-        id: "4",
-        productType: "Deep Eye Therapy",
-        Price: 680,
-        description: "Used day and night",
-        image:
-          "https://i.postimg.cc/zvtQKHL7/eye-complex-stylized-optimized-900x.webp",
-      },
-      {
-        id: "5",
-        productType: "Toner",
-        Price: 750,
-        description: "Used day and night",
-        image:
-          "https://i.postimg.cc/3rgfvK3c/white-tea-toner-stylized-opt-720x-no5.jpg",
-      },
-      {
-        id: "6",
-        productType: "Fine SKN All in One",
-        Price: 7750,
-        description: "Used day and night",
-        image: "https://i.postimg.cc/Z5r9q8vr/all-5.webp",
-      },
-    ])
-  );
-
-  const fineWrapper = document.querySelector("[data-checkout]");
+let cartItems = JSON.parse(localStorage.getItem("cart")) || [];      // if theres nothing in the cart -- empty array
 
 function ViewProducts() {
-  fineWrapper.innerHTML = "";
+  tableDisplay.innerHTML = "";
   try {
-    if (products) {
-      products.forEach((product) => {
-        fineWrapper.innerHTML += 
-        `<div class="card d-flex justify-content-center" w-25>
-                <img src="${product.image}" class="card-img-top" alt=${
-          product.id
-        }>
-                <div class="card-body">
-                  <h5 class="card-title">${product.productType}</h5>
-                  <p class="card-text">${product.Price}.</p>
-                  <button type='button' class="btn btn-primary" onclick='add2cart(${JSON.stringify(
-                    product
-                  )})' >Add to Cart</button>
-                </div>
-              </div>`;
-      });
-    } else {
-      fineWrapper.innerHTML += `<div class="d-flex justify-content-center">
-            <div class="spinner-border" role="status">
-              <span class="visually-hidden">Loading...</span>
-            </div>
-          </div>`; // add spinner
-    }
-  } catch {
-    error - message;
+    let shoppingItems = Object.groupBy(cartItems, (item) => item.id); // grouping the objects into an array
+    for (let key in shoppingItems) {                           //the shooping items will display the multiple objects/items added to cart
+      tableDisplay.innerHTML += ` 
+      <tr>
+          <th scope="row">${shoppingItems[key][0].id}</th>     
+          <td>
+          <img src="${shoppingItems[key][0].image}" 
+          class="img-fluid w-25" alt="${shoppingItems[key][0].productType}">
+          </td>
+          <td>${shoppingItems[key].length}</td>
+          <td>R${shoppingItems[key][0].Price}</td>
+          <td>R${eval(
+            `${shoppingItems[key][0].Price} * ${shoppingItems[key].length}`
+          )} 
+          </td>
+         </tr>
+          `;
+    }          
+    //the 0 indicates the index without duplicating the arrays
+
+
+// create a variable that displays the amount 
+
+
+
+    // if (cartItems) {
+    //   cartItems.forEach((product) => {
+    //     tableDisplay.innerHTML +=
+    //     ` <tr>
+    //            <th scope="row">${product.id}</th>
+    //            <td>${product.productType}</td>
+    //            <td>${product.price}</td>
+    //            <td>Action</td>
+    //      </tr>
+    //       `;
+    //   });
+    // } else {
+    //   tableDisplay.innerHTML += `<div class="d-flex justify-content-center">
+    //         <div class="spinner-border" role="status">
+    //           <span class="visually-hidden">Loading...</span>
+    //         </div>
+    //       </div>`; // add spinner
+    // }
+  } catch (e) {
+    // error - message;
   }
 }
 ViewProducts();
-
-// function ViewCheckout() {
-//   tableDisplay.innerHTML = "";
-//   try {
-//     if (table) {
-//       table.forEach((product) => {
-//         tableDisplay.innerHTML += `                
-//                   <td><img src ="${product.image}" alt=${product.id}></td>
-//                   <td>${product.productType}</td>
-//                   <td>${product.price}</td>`;
-//       });
-//       console.log(products);
-//     } else {
-//       tableDisplay.innerHTML += `<div class="d-flex justify-content-center">
-//             <div class="spinner-border" role="status">
-//               <span class="visually-hidden">Loading...</span>
-//             </div>
-//           </div>`; // spinner
-//     }
-//   } 
-//   catch {
-//     tableDisplay.innerHTML = "Please try again later";
-// }
-// }
-// ViewCheckout();
